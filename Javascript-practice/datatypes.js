a147a5f8fe28067550a4743f68f3562239194ec0
Mohammed-waqas <159753610+Mohammed-waqas@users.noreply.github.com>
2024-04-25 08:52:07 +0000

    Define datatypes

--- Javascript-practice/datatypes.js
+++ Javascript-practice/datatypes.js
@@ -10,7 +10,22 @@ Non-Premative Datatypes
 Premative Datatypes
 Number, String, Bolean, BigInt, Null, Undefine, Symbol=> unique,  
 */
+let Number = 234;
+let String = "Waqas";
+let Bolean = True;
+let Ab = null;
+let Symbol= Symbol;
+let Waqas;
+
+
 /* 
 Non-Premative Datatypes
 Object, Array 
 */
+let Student ={
+    name: "Waqas",
+    age : 25,
+    class: "Mern"
+}
+
+let Array=[1,2,3,3,4]
