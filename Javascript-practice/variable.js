aa7b4b4358abf81d6e355a1490755f2b1857d0f0
Mohammed-waqas <159753610+Mohammed-waqas@users.noreply.github.com>
2024-04-25 08:16:41 +0000

    variable concept

--- Javascript-practice/variable.js
+++ Javascript-practice/variable.js
@@ -1,1 +1,13 @@
-console.log("Waqas")
+const AccountId= 2345;
+let AccountPassword="2334";
+var AccountEmail="waqas@gmail.com"
+accountCity="Lahore";
+let accountState;
+
+//AccountId=342 //not allowed
+ //AccountPassword="waqas"
+
+ /* Var not preffer
+ because issue of block scope and functional scope
+ */ 
+console.table([AccountId, AccountPassword, AccountEmail, accountCity,accountState])
