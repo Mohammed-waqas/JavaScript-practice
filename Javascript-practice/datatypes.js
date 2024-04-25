d3e3b6ff74d4154476c63a3ad163f69aaa5fef81
Mohammed-waqas <159753610+Mohammed-waqas@users.noreply.github.com>
2024-04-25 08:45:26 +0000

    Datatypes

---
+++ Javascript-practice/datatypes.js
@@ -0,0 +1,16 @@
+"use strict";// treat all js code as newer version
+//alert= (3+3 ) // we are using nodejs not browser
+
+//Types of DataTypes
+/* 
+Premative Datatypes
+Non-Premative Datatypes
+*/
+/* 
+Premative Datatypes
+Number, String, Bolean, BigInt, Null, Undefine, Symbol=> unique,  
+*/
+/* 
+Non-Premative Datatypes
+Object, Array 
+*/
