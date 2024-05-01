233ffdc4608e772be36ea0cba379a9f01e2e3676
Mohammed-waqas <159753610+Mohammed-waqas@users.noreply.github.com>
2024-05-01 14:40:53 +0000

    postfix and prefix

--- Javascript-practice/conversion_Operation.js
+++ Javascript-practice/conversion_Operation.js
@@ -31,7 +31,17 @@ let str2 = " Waqas";
 let str3 = str1+str2;
 //console.log(str3);
 
-console.log("1" + 2);
+/* console.log("1" + 2);
 console.log(1 + "2");
 console.log(1 + 2 + "2");
-console.log("1" + 2+ 2);
+console.log("1" + 2+ 2); */
+
+let x = 3;
+const y = x++;
+// x is 4; y is 3
+//console.log(x,y);
+
+let x2 = 3n;
+const y2 = ++x2;
+// x2 is 4n; y2 is 4n
+console.log(x2, y2);
