52857834dd51d95f54a4286562d129a9146d6a54
Mohammed-waqas <159753610+Mohammed-waqas@users.noreply.github.com>
2024-05-01 09:44:17 +0000

    Conversion Operation

---
+++ Javascript-practice/conversion_Operation.js
@@ -0,0 +1,26 @@
+//let Score = "33abc";
+//let Score = null;
+//let Score = undefined;
+//let Score = true;
+//let ValueInNumber = Number(Score);
+//console.log(typeof ValueInNumber);
+//console.log(ValueInNumber);
+
+// "33" => 33;
+// "33abc" => NaN;
+// true => 1; false => 0;
+
+let isLogedIn = "waqas";
+
+let BoleanIn = Boolean(isLogedIn)
+//console.log(BoleanIn);
+
+// 1 => true; 0 => false;
+// "" => false ;
+// "waqas" => true
+
+let stringNumber = 33;
+
+let someNumber = String(stringNumber);
+console.log(someNumber);
+console.log(typeof someNumber);
