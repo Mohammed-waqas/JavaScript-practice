58125df14a30fe8db5079ae9a82745f112981a22
Mohammed-waqas <muhammeddwaqas@gmail.com>
2024-06-04 06:53:36 -0700

    Practice of Variable, Var, Let, Const

--- variable-datatypes.js
+++ variable-datatypes.js
@@ -1,1 +1,15 @@
-console.log("Muhammad Waqas")
+var fullName = "Muhammad waqas";
+var fullName = "Muhammad zawr";
+//console.log(fullName);
+
+// var : Variable can be re-declared & updated. A global scope variable.
+
+let age = 25;
+    age = 30;
+//console.log(age);
+
+// let : Variable cannot be re-declared but can be updated. A block scope variable.
+
+const PI=3.14;
+console.log(PI);
+// const : Variable cannot be re-declared or updated. A block scope variable.
