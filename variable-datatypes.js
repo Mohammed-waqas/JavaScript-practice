5154580edfe58500569d741ca4a6956d27fad920
Mohammed-waqas <muhammeddwaqas@gmail.com>
2024-06-04 07:14:21 -0700

    Practice of Datatypes

--- variable-datatypes.js
+++ variable-datatypes.js
@@ -13,3 +13,28 @@ let age = 25;
 const PI=3.14;
 console.log(PI);
 // const : Variable cannot be re-declared or updated. A block scope variable.
+
+//Data Types in Js
+//Primitive Datatypes = Number, String, Bolean , BigInt, Symbol, Undefine , Null
+//Non Primitive Datatypes = Object (Function, Array)
+
+/* let Name = "Muhammad Waqas";
+console.log(typeof Name)
+ */
+
+/* isFollow = false;
+console.log(typeof isFollow) */
+
+/* let x = null;
+console.log( x)
+console.log(typeof x) */
+
+const Student ={
+    Name : "Muhammad Waqas",
+    age : 24,
+    cgpa : 2.4,
+    ispassed: true
+}
+//Student["age"] = Student["age"] + 1;
+Student["Name"] = "Muhammad Zawar";
+console.log(Student["Name"]);
